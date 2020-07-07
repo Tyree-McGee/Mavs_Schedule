6c77153a32a9a1d9fe8f553a4c89154737ee69bf
Tyree McGee <ut10rocks@gmail.com>
2020-07-07 14:59:55 -0500

    Update SettingsActivity.java

--- app/src/main/java/com/example/mavs_schedule_2019_2020/SettingsActivity.java
+++ app/src/main/java/com/example/mavs_schedule_2019_2020/SettingsActivity.java
@@ -39,7 +39,6 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
-            // addPreferencesFromResource(R.xml.root_preferences); // ?????
         }
     }
 }
